2e135247491cfc7355e6fa17ffd7f195ca43bd25
David Bonillas <dave@lulus.com>
2014-04-22 09:51:16 -0700

    new sound manager 2

--- web/js/player.js
+++ web/js/player.js
@@ -1,46 +1,29 @@
 var lastid = '1';
 soundManager.url = '/swf/'
-function play_tune(url){
-  $.getJSON(url, function(data){
-    $('#a_'+data.id).removeClass('p_link').addClass('playing');
+
+var lastid = '1';
+function play_tune(request, json){
+
+    $('a_'+json.id).removeClassName('p_link');
+    $('a_'+json.id).addClassName('playing');
     if(lastid != '1'){
-      $('#'+lastid).removeClass('playing').addClass('p_link');
+        $(lastid).removeClassName('playing');
+        $(lastid).addClassName('p_link');
     }
-    
-    if($('#sm2-container')){
-      soundManager.stopAll();
-    }
-    
-    if ($('#song').length) {
-      $('#song').remove();
-    }
-    if(data.mpa){
-      $('#footer').append('<div id="song"></div>');
-      if(lastid != $('#a_'+data.id).attr('id')){
-        lastid = $('#a_'+data.id).attr('id');
-        var quickTimePlayer = QT_GenerateOBJECTText_XHTML(
-        data.mp3, '1', '1', '',
-        'emb#NAME', 'qsound_'+lastid,
-        'obj#id', 'qsound_'+lastid,'autoplay', 'true',
-        'EnableJavaScript', 'true'
-        );
-        $('#song').html(quickTimePlayer);
-      }else{
-        lastid = 1;
-      }
+
+    soundManager.stopAll();
+
+    var mySound = soundManager.createSound({
+        id: 'a_'+json.id,
+        url: json.mp3
+    });
+    soundManager.stopAll();
+
+    if(lastid != $('a_'+json.id).id){
+        lastid = $('a_'+json.id).id;
+        mySound.play();
     }else{
-      var mySound = soundManager.createSound({
-        id: 'a_'+data.id,
-        url: data.mp3
-      });
-      soundManager.stopAll();
-  
-      if(lastid != $('#a_'+data.id).attr('id')){
-          lastid = $('#a_'+data.id).attr('id');
-          mySound.play();
-      }else{
-          lastid = '1';
-      }
+        lastid = '1';
     }
-  });
+
 }
